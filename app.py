f94f972051874fafac5d8fb02c70fd09198b4116
Kimberly Tsiang <ktsiang@wesleyan.edu>
2020-05-01 14:15:12 -0400

    add flask cors

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 import pusher
 
 app = Flask(__name__)
+CORS(app)
+
 app.config.from_object(Config)
 
 db=SQLAlchemy(app)
